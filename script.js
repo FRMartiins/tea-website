dac26acce944b444e81589e2710cf77e3e00c8ca
FRMartiins <frmartiins@gmail.com>
2024-03-10 20:43:20 +1100

    second commint

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 AOS.init();
+const menu = document.querySelector('.menu');
+const NavMenu = document.querySelector('.header-menu');
+
+menu.addEventListener('click', ()=>{
+    menu.classList.toggle('ativo');
+    NavMenu.classList.toggle('ativo');
+})
